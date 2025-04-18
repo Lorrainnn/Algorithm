62184cb412eb5db512efe19a12d8fa40edbd91c1
Lorrainnn <jixual3@uci.edu>
2025-04-17 19:08:45 -0700

    finish

--- shell_sort5.py
+++ shell_sort5.py
@@ -1,1 +1,19 @@
+from shell_sort_common import common
+
 #The A003462 sequence, in reverse order, starting from the largest value less than n, down to 1.
+def shell_sort5(nums: list[int]):
+    n = len(nums)
+    
+    gaps = []
+    for i in range(n-1,0,-1):
+        gaps.append(i)
+
+    common(gaps,nums)
+
+
+
+
+if __name__=="__main__":
+    nums = [1,3,5,10,5,2,90,100,34]
+    shell_sort5(nums)
+    print(nums)
