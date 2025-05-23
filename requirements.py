c7d73d2b1ee1d76e0c8b2dc3f1e6657225ae8403
Lorrainnn <jixual3@uci.edu>
2025-05-23 13:03:18 -0700

    set up files

--- requirements.py
+++ requirements.py
@@ -1,22 +1,34 @@
 # Import each one of your sorting algorithms below as follows:
 # Feel free to comment out these lines before your algorithms are implemented.
-from insertion_sort import insertion_sort
-from tim_sort import tim_sort
-from shell_sort1 import shell_sort1
-from shell_sort2 import shell_sort2
-from shell_sort3 import shell_sort3
-from shell_sort4 import shell_sort4
-from shell_sort5 import shell_sort5
+from graph import Graph # type: ignore
+from graph_algorithms import get_diameter # type: ignore
+from graph_algorithms import get_clustering_coefficient # type: ignore
+from graph_algorithms import get_degree_distribution # type: ignore
 
-# Please read the below carefully:
+# Details about Gradescope submission:
 
-# - Each sorting algorithm should be implemented in its own file.
+# - The graph class should be stored in separate files from the three graph algorithms.
 # - No file should include anything outside of standard Python libraries.
 # - Functions should be tested using Python 3.6+ on a Linux environment.
-# - Each function should modify the input list so that it is sorted upon completion.
+# - The submission should either be the files themselves, or a zip file not containing any directories.
 
-# Note:
-#   If your Shellsort and/or other sort variants largely use the same code,
-#   you may choose to implement them in a single file, and import them as follows:
-# from shell_sort import shell_sort1, shell_sort2, shell_sort3, shell_sort4, shell_sort5
 
+# Explanations for Graph public member functions
+#
+# init(): initialize the graph a given number of nodes and given edges.
+#         edges are represented as tuples (u, v), where each edge is between node with index u and
+#         node with index v.
+#         the autograder will never add duplicate edges to the graph.
+# get_num_nodes(): returns the number of nodes in the graph.
+# get_num_edges(): returns the number of edges in the graph.
+# get_neighbors(): given a node index, return an iterable type over the collection of its neighbors.
+#                  the iterable type can be a list, set, generator, etc.
+#                  each neighbor should appear exactly once.
+
+# Explanations for graph algorithm functions
+#
+# get_diameter(): return the approximate graph diameter using a heuristic function.
+# get_clustering_coefficient(): return the graph's global clustering coefficient.
+# get_degree_distribution(): returns a dictionary representing the degree distribution of the graph.
+#                            the keys are the degree, and the values is the number of nodes with that
+#                            degree.
